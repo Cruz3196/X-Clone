e7aeb90f08ba6b5667940cb34c23624843233de1
test <test>
2025-04-22 21:55:26 -0700

    update

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,7 +1,51 @@
+import User from "../models/user.models.js";
+import bcrypt from "bcryptjs";
+
 export const signup = async (req, res) => {
-    res.json({ data: "You hit the signup endpoint",
-    });
-}
+    try{
+
+        const {fullName, username,email, password} = req.body;
+
+        // creating an email for the user 
+        const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+        if (!emailRegex.test(email)) {
+                return res.status(400).json({ error: 'Invalid email format' });
+            }
+
+        // checking to see if the user exists 
+        const existingUser = await User.findOne({ username });
+            if(existingUser){
+                return res.status(400).json({error: "User is already taken"});
+            }
+
+        // checking for exisitng email 
+        const existingEmail = await User.findOne({ email });
+            if(existingEmail){
+                return res.status(400).json({error: "Email is already in use"});
+            }
+        
+        // hashing the password 
+        const salt = await bcrypt.genSalt(10); // the bigger this value gets, the longer this will take to complete
+        const hashedPassword = await bcrypt.hash(password, salt);
+        
+        // creating new user 
+        const newUser = new User({
+            fullName,
+            username,
+            email,
+            password: hashedPassword
+        })
+
+        if(newUser){
+            generateTokenAndSetCookie(newUser._id,res)
+        }else{
+
+        }
+
+    }catch (error){
+
+    }
+};
 export const login = async (req, res) => {
     res.json({ data: "You hit the login endpoint",
     });
--- backend/server.js
+++ backend/server.js
@@ -6,7 +6,9 @@ import connectMongDB from "./db/connectMongoDB.js";
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 3001; 
+const PORT = process.env.PORT || 3001;
+
+app.use(express.json()); // to parse req.body
 
 app.use("/api/auth", authRoutes);
 
@@ -14,3 +16,5 @@ app.listen(PORT, () => {
     console.log(`Server is running on this port ${PORT}`);
     connectMongDB(); 
 });
+
+//middleware is just a function that runs between request and responses 
