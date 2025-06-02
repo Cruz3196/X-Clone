2233e5955870f240231e80527cab1fc9dea79d12
Cruz3196 <cruzjos9619@gmail.com>
2025-06-02 09:00:19 -0700

    protectroute updated

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 import authRoutes from "./routes/auth.routes.js";
 import connectMongDB from "./db/connectMongoDB.js";
+import cookieParser from "cookie-parser";
 
 dotenv.config();
 
@@ -10,6 +11,8 @@ const PORT = process.env.PORT || 3001;
 
 app.use(express.json()); // to parse req.body
 app.use(express.urlencoded({ extended: true })); // to parse the form data 
+
+app.use(cookieParser()); 
 
 app.use("/api/auth", authRoutes);
 
