aae0abcebaba996f7a9199960137071e2f71efbd
Cruz3196 <cruzjos9619@gmail.com>
2025-06-04 22:06:05 -0700

    update

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,14 @@
+// packages
 import express from "express";
 import dotenv from "dotenv";
-import authRoutes from "./routes/auth.routes.js";
-import connectMongDB from "./db/connectMongoDB.js";
 import cookieParser from "cookie-parser";
+
+// routes 
+import authRoutes from "./routes/auth.routes.js";
+import userRoutes from "./routes/user.routes.js";
+
+// util
+import connectMongDB from "./db/connectMongoDB.js";
 
 dotenv.config();
 
@@ -15,6 +21,7 @@ app.use(express.urlencoded({ extended: true })); // to parse the form data
 app.use(cookieParser()); 
 
 app.use("/api/auth", authRoutes);
+app.use("/api/auth", userRoutes);
 
 app.listen(PORT, () => {
     console.log(`Server is running on this port ${PORT}`);
