f17d7b9aa101e3a4b0c9abf112f5bbb9a7b4d943
Cruz3196 <cruzjos9619@gmail.com>
2025-06-04 23:11:28 -0700

    followUnfollowUser controller update

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -14,3 +14,35 @@ export const getUserProfile = async (req, res) => {
         res.status(500).json({ error: error.message });
     }
 };
+
+export const followUnfollowUser = async (req, res) => {
+    try{
+        const {id} = req.params;
+        const userToModify = await User.findById(id);
+        const currentUser = await User.findById(req.user._id);
+
+        if(id === req.user._id){
+            return res.status(400).json({error: "You cannot follow yourself"});
+        }
+
+        if(!userToModify || !currentUser) return res.status(400).json({error: "User not found"}); 
+
+        const isFollowing = currentUser.following.includes(id);
+
+        if(isFollowing){
+            //unfollow the user
+            await User.findByIdAndUpdate(id, { $pull: { followers: req.user._id } });
+            await User.findByIdAndUpdate(req.user._id, { $pull: { following: id } });
+            res.status(200).json({message: "User unfollowed"});
+        } else{
+            //follow the user
+            await User.findByIdAndUpdate(id, { $push: { followers: req.user._id } }); 
+            await User.findByIdAndUpdate(req.user._id, { $push: { following: id } });
+            // send a notification to the user 
+            res.status(200).json({message: "User followed"});
+        }
+    }catch (error){
+        console.log("Error in getUserProfile: ", error.message);
+        res.status(500).json({ error: error.message });
+    }
+}
--- backend/routes/user.routes.js
+++ backend/routes/user.routes.js
@@ -1,6 +1,6 @@
 import express from "express";
 import { protectRoute } from "../middleware/protectRoute.js";
-import { getUserProfile } from "../controllers/user.controller.js";
+import { getUserProfile, followUnfollowUser } from "../controllers/user.controller.js";
 
 
 const router = express.Router();
@@ -9,7 +9,7 @@ router.get("/profile/:username",protectRoute ,getUserProfile);
 
 // router.get("/suggested",protectRoute,getUserProfile);
 
-// router.post("/follow/:id",protectRoute,followUnfollowUser);
+router.post("/follow/:id",protectRoute,followUnfollowUser);
 
 // router.post("/update",protectRoute,updateUserProfile);
 
