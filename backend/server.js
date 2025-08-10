a6c0ceb4b636b9c1f55030bf0444b56c972ca1e5
Jose Cruz <159391963+Cruz3196@users.noreply.github.com>
2025-08-09 18:45:51 -0700

    updated dependency

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,6 @@
 // packages
+// the path module is a built in node module 
+import path from "path";
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
@@ -24,6 +26,9 @@ cloudinary.config({
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+// *configuring the backend to the front end 
+const __dirname = path.resolve();
+
 app.use(express.json({limit: "5mb"})); // to parse req.body & allowing large files to be uploaded. also this shouldn't be too large if an attacker sends a large request to your application, then your server might crash.which is called DoS (DoS = Denial of Service)
 app.use(express.urlencoded({ extended: true })); // to parse the form data 
 
@@ -33,6 +38,15 @@ app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/posts", postRoutes);
 app.use("/api/notifications", notificationRoutes);
+
+//* navigating the user to the react application
+if (process.env.NODE_ENV === "production"){
+    app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+    app.get("*", (req,res) => {
+        res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+    })
+}
 
 app.listen(PORT, () => {
     console.log(`Server is running on this port ${PORT}`);
